29f74f7a158077a86ba6dcaefe46a9cbc1c0fe32
jovirc <johanrc95@gmail.com>
2022-05-19 21:51:41 -0400

    test

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './Dashboard.style';
 
 const Dashboard = () => {
-  return <Container>Dachboard</Container>;
+  return <Container>Dachboard </Container>;
 };
 
 export default Dashboard;
